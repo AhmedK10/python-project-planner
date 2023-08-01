a69cefa527d0520c2882f86b59feea877bdc46d8
Ahmed Khogali <ahmed.khog@outlook.com>
2023-08-01 18:14:19 +0300

    bug fixes

--- ProjectPlanner/planner.py
+++ ProjectPlanner/planner.py
@@ -1,10 +1,21 @@
 import csv
-import tkinter
 from collections import namedtuple
-from tkinter,filedialog import askopenfilename
+import tkinter
+from tkinter.filedialog import askopenfilename
 
 #making a named tuple to make accessing data later easier
 Task_tuple = namedtuple("Task_tuple", ["title", "length", "prereq"])
+
+
+def main():
+    root = tkinter.Tk()
+    root.title("Project Planner")
+    open_button = tkinter.Button(root, text="Open project", command=open_project)
+    open_button.pack(side="top")
+    canvas = tkinter.Canvas(root, width=800, height=400, bg="white")
+    canvas.pack(side="bottom")
+    tkinter.mainloop()
+
 
 def task_reader(file):
     # creating an empty dictionary to store the data from the csv
@@ -54,16 +65,22 @@ def draw_chart(tasks, canvas, row_height=40, title_width=300, line_height=40, da
         x = title_width + week_number * week_width
         canvas.create_line(x, 0, x, height, fill="grey")
         canvas.create_text(x + week_width/2, row_height/2, text=f"Week {week_number + 1}", font=("Helvetica", font_size, "bold"))
+        start_days = task_orderer(tasks)
+        y = row_height
+        for task_number in start_days:
+            task = tasks[task_number]
+            canvas.create_text(title_indent, y + row_height / 2, text=task.title, anchor=tkinter.W, font=("Helvetica", font_size))
+            bar_x = title_width + start_days[task_number] * day_width
+            bar_y = y + (row_height - bar_height) / 2
+            bar_width = task.length * day_width
+            canvas.create_rectangle(bar_x, bar_y, bar_x + bar_width, bar_y + bar_height, fill="red")
+            y = y + row_height
 
 def open_project():
-    filename = askopenfilename(title="Open Project", iniialdir=".", filetypes=[("CSV Document", "*.csv")])
+    filename = askopenfilename(title="Open Project", initialdir=".", filetypes=[("CSV Document", "*.csv")])
     tasks = task_reader(filename)
     draw_chart(tasks, canvas)
 
-root = thinker.Tk()
-root.title("Project Planner")
-open_button = tkinter.Button(root, text="Open project", command=open_project)
-open_button.pack(side="top")
-canvas = tkinter.Canvas(root, width=800, height=400, bg="white")
-canvas.psck(side="bottom")
-tkinter.mainloop()
+
+if __name__ == "__main__":
+    main()
