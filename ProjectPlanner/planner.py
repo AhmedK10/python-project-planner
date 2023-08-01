dddf74462c13ae16c5b4121d616ec6344e02fd22
Ahmed Khogali <ahmed.khog@outlook.com>
2023-08-01 14:42:55 +0300

    finished reading the csv data and converting to a useful format

--- ProjectPlanner/planner.py
+++ ProjectPlanner/planner.py
@@ -1,4 +1,8 @@
+from collections import namedtuple
 import csv
+
+#making a named tuple to make accessing data later easier
+Task_tuple = namedtuple("Task_tuple", ["title", "length", "prereq"])
 
 def task_reader(file):
     # creating an empty dictionary to store the data from the csv
@@ -7,10 +11,10 @@ def task_reader(file):
         num = int(row[0])
         title = row[1]
         length = float(row[2])
-        prereq = row[3]
+        prereq = set(map(int, row[3].split))
 
         # since I dont want the data from the csv to change, I am storing in a tuple
-        dict_tasks[num] = (title, length, prereq)
+        dict_tasks[num] = Task_tuple(title, length, prereq)
 
     #returns all data
     return dict_tasks
